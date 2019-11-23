a800a8914821b71d753c0f6ba91aa3629b7bd6a7
Aluno <Aluno@LAB02-PC15>
2019-11-22 22:12:21 -0200

    Analise Curricular

--- ProcessoSeletivo/src/Selecao/AnaliseCurricular.java
+++ ProcessoSeletivo/src/Selecao/AnaliseCurricular.java
@@ -1,5 +1,10 @@
 package Selecao;
 
+import java.io.File;
+import java.util.Calendar;
+
 public interface AnaliseCurricular {
+	
+	public void analiseCurricular(String nome, File foto, String contribuicoes, String experiencias, Calendar InicioCurso, Calendar finalCurso, File videoCurriculo);
 
 }
